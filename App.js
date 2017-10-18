50cb0b4f5545e9d5a0736fbb7551ae3f0c2cccc6
heather-walker <hrw@caselle.com>
2017-10-17 23:08:09 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import Modal from 'react-modal';
 import logo from './podium_logo.png';
 import phone from './phone_hand.png';
 import './App.css';
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    borderRadius          : '20px',
+    backgroundColor       : 'lightgray'
+  }
+};
+
 class App extends Component {
+
+  constructor() {
+   super();
+
+   this.state = {
+     modalIsOpen: true
+   };
+
+   this.openModal = this.openModal.bind(this);
+   this.afterOpenModal = this.afterOpenModal.bind(this);
+   this.closeModal = this.closeModal.bind(this);
+ }
+
+ openModal() {
+   this.setState({modalIsOpen: true});
+ }
+
+ afterOpenModal() {
+   // references are now sync'd and can be accessed.
+   this.subtitle.style.color = '#878f92';
+ }
+
+ closeModal() {
+   this.setState({modalIsOpen: false});
+ }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="po_header floatleft" alt="logo" />
-          <h1 className="po_header floatleft" style={{"margin-top": 5}}>PODIUM</h1>
+          <h1 className="po_header floatleft" style={{"marginTop": 5}}>PODIUM</h1>
           <ul className="floatright">
             <li><a href="#">PRODUCT</a></li>
             <li><a href="#">RESOURCES</a></li>
@@ -17,10 +59,29 @@ class App extends Component {
             <li><a href="#">LOGIN</a></li>
           </ul>
         </header>
-          <img src={phone} className=" section_one_body floatleft"/>
+          <img src={phone} className=" section_one_body floatleft" alt="mobile"/>
           <div className=" section_one_body"> Some text with a class added for cool styles </div>
           <div className=" section_one_body"> Some more text with a different class for even more cool styles </div>
-          <button className="watch_demo_button "> ▶ WATCH DEMO </button>
+          <button className="watch_demo_button " id="openModal" onClick={this.openModel}> ▶ WATCH DEMO </button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+          <a href="#" className="floatright close_modal" onClick={this.closeModal}>x</a>
+          <div style={{"textAlign": "center"}}>
+          <img src={logo} className="po_header" alt="logo" />
+          <p ref={subtitle => this.subtitle = subtitle}>Enter your email to watch</p>
+          <form>
+            <input className="modal_input_text" placeholder="ENTER.EMAIL@COMPANY.COM"/>
+            <div style={{marginTop: 20}}>(no spam, we promise!)</div>
+            <button className="watch_demo_button modal">▶ WATCH DEMO </button>
+          </form>
+          </div>
+
+        </Modal>
           <button className="learn_more_button "> LEARN MORE </button>
 
       </div>
