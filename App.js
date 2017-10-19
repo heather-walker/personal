c650aab7530c96ca130ed611c50c84927b09f221
heather-walker <hrw@caselle.com>
2017-10-18 23:24:07 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Modal from 'react-modal';
 import logo from './podium_logo.png';
 import phone from './phone_hand.png';
+import close from './close-icon.png';
 import './App.css';
 
 const customStyles = {
@@ -14,7 +15,18 @@ const customStyles = {
     marginRight           : '-50%',
     transform             : 'translate(-50%, -50%)',
     borderRadius          : '20px',
-    backgroundColor       : 'lightgray'
+    backgroundColor       : 'lightgray',
+    overflow              : 'visible',
+
+
+  },
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    background        : 'linear-gradient(rgba(0,0,0,.85), rgba(255,255,255,.85))',
   }
 };
 
@@ -24,7 +36,7 @@ class App extends Component {
    super();
 
    this.state = {
-     modalIsOpen: true
+     modalIsOpen: false
    };
 
    this.openModal = this.openModal.bind(this);
@@ -38,7 +50,9 @@ class App extends Component {
 
  afterOpenModal() {
    // references are now sync'd and can be accessed.
-   this.subtitle.style.color = '#878f92';
+   this.subtitle.style.color = '#033B5C';
+   this.subtitle.style.fontWeight = '100';
+   this.subtitle.style.letterSpacing = '.5px';
  }
 
  closeModal() {
@@ -59,30 +73,38 @@ class App extends Component {
             <li><a href="#">LOGIN</a></li>
           </ul>
         </header>
-          <img src={phone} className=" section_one_body floatleft" alt="mobile"/>
-          <div className=" section_one_body"> Some text with a class added for cool styles </div>
-          <div className=" section_one_body"> Some more text with a different class for even more cool styles </div>
-          <button className="watch_demo_button " id="openModal" onClick={this.openModel}> ▶ WATCH DEMO </button>
+        <div style={{ height: 585, borderBottom:"1px solid lightgray"}}>
+          <img src={phone} className=" section_one_body floatleft" style={{marginTop: "-40px"}} alt="mobile"/>
+          <div className=" section_one_body"> Engage with customers on their terms. </div>
+          <div className=" section_one_body part_two"> Podium takes all the guesswork out of engaging with your customers.
+          We take a non-invasive approach to gathering feedback and building relationships. </div>
+          <button className="watch_demo_button " id="openModal" onClick={this.openModal}> ▶ WATCH DEMO </button>
           <Modal
             isOpen={this.state.modalIsOpen}
             onAfterOpen={this.afterOpenModal}
             onRequestClose={this.closeModal}
             style={customStyles}
-            contentLabel="Example Modal"
           >
-          <a href="#" className="floatright close_modal" onClick={this.closeModal}>x</a>
+          <img src={close} className=" close_icon" alt="close" onClick={this.closeModal}/>
           <div style={{"textAlign": "center"}}>
           <img src={logo} className="po_header" alt="logo" />
-          <p ref={subtitle => this.subtitle = subtitle}>Enter your email to watch</p>
+          <p ref={subtitle => this.subtitle = subtitle}>ENTER YOUR EMAIL TO WATCH</p>
           <form>
             <input className="modal_input_text" placeholder="ENTER.EMAIL@COMPANY.COM"/>
             <div style={{marginTop: 20}}>(no spam, we promise!)</div>
-            <button className="watch_demo_button modal">▶ WATCH DEMO </button>
+            <button className="watch_demo_button modal" >
+            <a href="https://youtu.be/U9t-slLl30E?t=5s" style={{color: "inherit"}}> ▶ WATCH DEMO </a>
+            </button>
           </form>
           </div>
-
         </Modal>
           <button className="learn_more_button "> LEARN MORE </button>
+          </div>
+          <div>
+          <div className=" section_one_body floatleft"> Get found. Get chosen. Get insights.
+          <button className="learn_reviews_button "> LEARN ABOUT REVIEWS </button>
+          </div>
+          </div>
 
       </div>
     );
